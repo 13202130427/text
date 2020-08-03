cd3735d352478677d2969d4bde6fc392cf5d0c78
xiaozheng <1067652489@qq.com>
2020-08-03 14:23:38 +0800

    我的第一个版本

---
+++ demo.php
@@ -0,0 +1,4 @@
+<?php
+require_once './vendor/autoload.php';
+use Text\Starfish\Tools;
+Tools::Hello();
---
+++ src/Text/Starfish/Tools.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Text\Starfish;
+
+
+class Tools
+{
+    public function __construct()
+    {
+
+    }
+    public static function Hello() {
+        echo "Hello world";
+    }
+}
